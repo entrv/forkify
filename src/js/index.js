a0ec74f31d2da948499d0885548c1094b560cf4d
entrv <entrvn7@gmail.com>
2020-01-18 00:55:15 +0900

    'struct'

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
-// Global app controller
-import num from './test'
-console.log('i am imported can do 1233333' + num)
+//fa57c242  application id
+//d1edbb4c8552cec56b76bfb9f993041e	— application key
+
+//import str from './models/Search'
+
+//import {add as a, multiply as m , ID } from './views/searchView'
+
+//import * as searchView from './views/searchView'
+
+//console.log(`Using11 imported funtion ${searchView.add(
+//    searchView.ID, 2)}   and 
+//${searchView.multiply(searchView.ID, 2)}
+//`)
+
+import axios from 'axios'
+
+async function getResults(query) {
+    axios()
+}
+
+getResults();
+
--- src/js/test.js
+++
@@ -1,3 +0,0 @@
-console.log('importred')
-
-export default 9944777700000;
